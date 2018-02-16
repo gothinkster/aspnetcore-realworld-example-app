156c02441ee9451a72520b32f71d61885029c826
Adam Hathcock <adam@hathcock.uk>
2018-02-16 15:49:56 +0000

    Oops should have used enumerable validator

--- src/Conduit/Infrastructure/ValidationPipelineBehavior.cs
+++ src/Conduit/Infrastructure/ValidationPipelineBehavior.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation;
 using MediatR;
@@ -7,22 +9,25 @@ namespace Conduit.Infrastructure
 {
     public class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
     {
-        private readonly IValidator<TRequest> _validator;
+        private readonly List<IValidator<TRequest>> _validators;
 
-        public ValidationPipelineBehavior(IValidator<TRequest> validators)
+        public ValidationPipelineBehavior(IEnumerable<IValidator<TRequest>> validators)
         {
-            _validator = validators;
+            _validators = validators.ToList();
         }
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             var context = new ValidationContext(request);
+            var failures = _validators
+                .Select(v => v.Validate(context))
+                .SelectMany(result => result.Errors)
+                .Where(f => f != null)
+                .ToList();
 
-            var result = await _validator.ValidateAsync(context, cancellationToken);
-
-            if (!result.IsValid)
+            if (failures.Count != 0)
             {
-                throw new ValidationException(result.Errors);
+                throw new ValidationException(failures);
             }
 
             return await next();
