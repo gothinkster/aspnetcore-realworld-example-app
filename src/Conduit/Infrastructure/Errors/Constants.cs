bde418233d7bd819e30344358529505f37eb245b
Adam Hathcock <adam@hathcock.co.uk>
2020-04-12 20:14:33 +0100

    Use const

--- src/Conduit/Infrastructure/Errors/Constants.cs
+++ src/Conduit/Infrastructure/Errors/Constants.cs
@@ -2,8 +2,8 @@
 {
     public static class Constants
     {
-        public static string NOT_FOUND = "not found";
-        public static string IN_USE = "in use";
+        public const string NOT_FOUND = "not found";
+        public const string IN_USE = "in use";
         public static readonly string InternalServerError = nameof(InternalServerError);
     }
 }
