b839b4ce48f9613ee83996bb8e863d6cafda11ce
Kamukama Denis <kampd80@gmail.com>
2023-08-17 09:06:34 +0300

    removed unused directives

--- src/Conduit/Startup.cs
+++ src/Conduit/Startup.cs
@@ -14,7 +14,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace Conduit
