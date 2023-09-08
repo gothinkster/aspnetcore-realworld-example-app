7c8c86e7e998d7f845bcac149ab46ce5e3d7d714
Kamukama Denis <kampd80@gmail.com>
2023-09-08 14:55:44 +0300

    migrated from Startup.cs to Program.cs

--- src/Conduit/Program.cs
+++ src/Conduit/Program.cs
@@ -1,17 +1,151 @@
 using Conduit;
-using Microsoft.AspNetCore.Hosting;
+using Conduit.Features.Profiles;
+using Conduit.Infrastructure.Security;
+using Conduit.Infrastructure;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using Conduit.Infrastructure.Errors;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+
+//var config = new ConfigurationBuilder()
+//    .AddEnvironmentVariables()
+//    .Build();
+
+//var host = new WebHostBuilder()
+//    .UseConfiguration(config)
+//    .UseKestrel()
+//    .UseUrls($"http://+:5000")
+//    .UseStartup<Startup>()
+//    .Build();
+
+//await host.RunAsync();
 
 // read database configuration (database provider + database connection) from environment variables
-var config = new ConfigurationBuilder()
-    .AddEnvironmentVariables()
-    .Build();
+//Environment.GetEnvironmentVariable(DEFAULT_DATABASE_PROVIDER)
+//Environment.GetEnvironmentVariable(DEFAULT_DATABASE_CONNECTION_STRING)
+var defaultDatabaseConnectionSrting = "Filename=realworld.db";
+var defaultDatabaseProvider = "sqlite";
 
-var host = new WebHostBuilder()
-    .UseConfiguration(config)
-    .UseKestrel()
-    .UseUrls($"http://+:5000")
-    .UseStartup<Startup>()
-    .Build();
+var builder = WebApplication.CreateBuilder(args);
 
-await host.RunAsync();
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
+builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(DBContextTransactionPipelineBehavior<,>));
+
+// take the connection string from the environment variable or use hard-coded database name
+var connectionString = defaultDatabaseConnectionSrting;
+// take the database provider from the environment variable or use hard-coded database provider
+var databaseProvider = defaultDatabaseProvider;
+//if (string.IsNullOrWhiteSpace(databaseProvider))
+//{
+//    databaseProvider = DEFAULT_DATABASE_PROVIDER;
+//}
+
+builder.Services.AddDbContext<ConduitContext>(options =>
+{
+    if (databaseProvider.ToLowerInvariant().Trim().Equals("sqlite", StringComparison.Ordinal))
+    {
+        options.UseSqlite(connectionString);
+    }
+    else if (databaseProvider.ToLowerInvariant().Trim().Equals("sqlserver", StringComparison.Ordinal))
+    {
+        // only works in windows container
+        options.UseSqlServer(connectionString);
+    }
+    else
+    {
+        throw new InvalidOperationException("Database provider unknown. Please check configuration");
+    }
+});
+
+builder.Services.AddLocalization(x => x.ResourcesPath = "Resources");
+
+// Inject an implementation of ISwaggerProvider with defaulted settings applied
+builder.Services.AddSwaggerGen(x =>
+{
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        BearerFormat = "JWT"
+    });
+
+    x.SupportNonNullableReferenceTypes();
+
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {   new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                    },
+                    Array.Empty<string>()}
+                });
+    x.SwaggerDoc("v1", new OpenApiInfo { Title = "RealWorld API", Version = "v1" });
+    x.CustomSchemaIds(y => y.FullName);
+    x.DocInclusionPredicate((version, apiDescription) => true);
+    x.TagActionsBy(y => new List<string>()
+                {
+                    y.GroupName ?? throw new InvalidOperationException()
+                });
+});
+
+builder.Services.AddCors();
+builder.Services.AddMvc(opt =>
+{
+    opt.Conventions.Add(new GroupByApiRootConvention());
+    opt.Filters.Add(typeof(ValidatorActionFilter));
+    opt.EnableEndpointRouting = false;
+})
+    .AddJsonOptions(opt => opt.JsonSerializerOptions.DefaultIgnoreCondition =
+          System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull);
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationClientsideAdapters();
+builder.Services.AddValidatorsFromAssemblyContaining<Startup>();
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+builder.Services.AddScoped<ICurrentUserAccessor, CurrentUserAccessor>();
+builder.Services.AddScoped<IProfileReader, ProfileReader>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddJwt();
+
+var app = builder.Build();
+
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseCors(builder =>
+    builder
+        .AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+
+app.UseAuthentication();
+app.UseMvc();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint
+app.UseSwagger(c => c.RouteTemplate = "swagger/{documentName}/swagger.json");
+
+// Enable middleware to serve swagger-ui assets(HTML, JS, CSS etc.)
+app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "RealWorld API V1"));
+
+app.Services.GetRequiredService<ConduitContext>().Database.EnsureCreated();
