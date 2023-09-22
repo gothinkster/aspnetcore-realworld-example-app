33afc35a66deb1e9094867447743b06605bd6fa3
Kamukama Denis <kampd80@gmail.com>
2023-09-22 16:07:46 +0300

    Added Articles,Comments,Favorites,Follower amd Profiles to minimal api

---
+++ src/Conduit/MinimalApi/Comments.cs
@@ -0,0 +1,29 @@
+using Conduit.Features.Comments;
+using Conduit.Infrastructure.Security;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace Conduit.MinimalApi
+{
+    public static class Comments
+    {
+        public static void RegisterCommentEndpoints(this WebApplication app)
+        {
+            app.MapPost("articles/{slug}/comments", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)] async (string slug,
+                [FromBody] Create.Model model, CancellationToken cancellationToken,
+               IMediator mediator) => await mediator.Send(new Create.Command(model, slug), cancellationToken));
+
+            app.MapGet("articles/{slug}/comments", async (string slug,
+               [FromBody] Create.Model model, CancellationToken cancellationToken,
+              IMediator mediator) => await mediator.Send(new List.Query(slug), cancellationToken));
+
+            app.MapDelete("articles/{slug}/comments/{id}", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)] async (string slug,
+                int id, CancellationToken cancellationToken, IMediator mediator) => await mediator.Send(new Delete.Command(slug, id), cancellationToken));
+
+        }
+    }
+}
+
---
+++ src/Conduit/MinimalApi/Profiles.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
+using Conduit.Features.Profiles;
+
+namespace Conduit.MinimalApi
+{
+    public static class Profiles
+    {
+        public static void RegisterProfileEndpoints(this WebApplication app) =>
+            app.MapGet("profiles/{username}", async (string username,
+                CancellationToken cancellationToken,
+                IMediator mediator)
+                => await mediator.Send(new Details.Query(username), cancellationToken));
+    }
+}
