d4aec14ce65930071abe4d21491d930fd0720e54
Kamukama Denis <kampd80@gmail.com>
2023-09-22 16:23:38 +0300

    Added User to minimal Api

---
+++ src/Conduit/MinimalApi/User.cs
@@ -0,0 +1,33 @@
+using MediatR;
+using System.Threading;
+using Microsoft.AspNetCore.Builder;
+using Conduit.Infrastructure;
+using Conduit.Features.Users;
+using Microsoft.AspNetCore.Mvc;
+using Conduit.Infrastructure.Security;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Conduit.MinimalApi
+{
+    public static class User
+    {
+        public static void RegisterUsersEndpoint(this WebApplication app)
+        {
+            app.MapGet("user", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)]
+            async (ICurrentUserAccessor currentUserAccessor, CancellationToken cancellationToken,
+              IMediator mediator) => await mediator.Send(new Details.Query(currentUserAccessor.GetCurrentUsername() ?? "<unknown>"), cancellationToken));
+
+            app.MapPut("user", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)]
+            async ([FromBody] Edit.Command command, CancellationToken cancellationToken,
+                IMediator mediator) => await mediator.Send(command, cancellationToken));
+
+            app.MapPost("users", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)]
+            async ([FromBody] Create.Command command, CancellationToken cancellationToken,
+               IMediator mediator) => await mediator.Send(command, cancellationToken));
+
+            app.MapPost("users/login", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)]
+            async ([FromBody] Login.Command command, CancellationToken cancellationToken,
+              IMediator mediator) => await mediator.Send(command, cancellationToken));
+        }
+    }
+}
