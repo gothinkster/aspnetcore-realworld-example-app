2e28eb5858fef5aa68fe12c45df6d2bdaa84652d
Kamukama Denis <kampd80@gmail.com>
2023-09-22 15:36:29 +0300

    Added route handling on Favroites and comments

--- src/Conduit/Endpoints/CommentsMinimalApi.cs
+++ src/Conduit/Endpoints/CommentsMinimalApi.cs
@@ -26,4 +26,4 @@ namespace Conduit.Endpoints
         }
     }
 }
-}
+
---
+++ src/Conduit/Endpoints/Favorites.cs
@@ -0,0 +1,23 @@
+using Conduit.Features.Favorites;
+using Conduit.Infrastructure.Security;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using System.Threading;
+
+namespace Conduit.Endpoints
+{
+    public static class Favorites
+    {
+        public static void RegisterFavoritesEndpoint(this WebApplication app)
+        {
+            app.MapPost("/articles/{slug}/favorite", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)] async (string slug,
+                 CancellationToken cancellationToken,
+               IMediator mediator) => await mediator.Send(new Add.Command(slug), cancellationToken));
+
+            app.MapDelete("/articles/{slug}/favorite", [Authorize(AuthenticationSchemes = JwtIssuerOptions.Schemes)] async (string slug,
+                CancellationToken cancellationToken, IMediator mediator) => await mediator.Send(new Delete.Command(slug), cancellationToken));
+
+        }
+    }
+}
