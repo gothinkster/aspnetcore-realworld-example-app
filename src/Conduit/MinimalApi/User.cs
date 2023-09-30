19cc76194d962519f5eb2f7be8bf29013dda6c92
kamukama Denis <kampd80@gmail.com>
2023-09-30 06:06:13 +0300

    Added validating User body on Create

--- src/Conduit/MinimalApi/User.cs
+++ src/Conduit/MinimalApi/User.cs
@@ -23,7 +23,7 @@ namespace Conduit.MinimalApi
                 IMediator mediator) => await mediator.Send(command, cancellationToken));
 
             app.MapPost("user",
-            async ([FromBody] Create.Command command, CancellationToken cancellationToken,
+            async ([Validate][FromBody] Create.Command command, CancellationToken cancellationToken,
                IMediator mediator) => await mediator.Send(command, cancellationToken));
 
             app.MapPost("user/login",
